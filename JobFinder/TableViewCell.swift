5ae0169ea5ee1350b5c1e2b55c4f1b831feedf04
Juan Renteria <jrenteria6223@gmail.com>
2021-01-19 00:22:57 -0600

    tableview cell

--- JobFinder/TableViewCell.swift
+++ JobFinder/TableViewCell.swift
@@ -9,16 +9,38 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    
+    var favorite = false
+    var favoriteButton = UIButton(type: .system)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        favoriteButton.setImage(UIImage(systemName: "star"), for: .normal)
+        favoriteButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        favoriteButton.tintColor = .orange
+        accessoryView = favoriteButton
+        
+        favoriteButton.addTarget(self, action: #selector(handleMarkAsFavorite), for: .touchUpInside)
     }
+    
+    @objc private func handleMarkAsFavorite() {
+        
+        if favorite == false {
+            favoriteButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
+            print("mark as favorite")
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+            favorite = !favorite
+        } else if favorite == true {
+            favoriteButton.setImage(UIImage(systemName: "star"), for: .normal)
+            print("not favorite")
 
-        // Configure the view for the selected state
+            favorite = !favorite
+        }
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
